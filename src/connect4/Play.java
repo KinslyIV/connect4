05f412e5ffce596f79f23ff5831a2584d2fadce8
KinslyIV <immatakuete@gmail.com>
2024-11-04 00:01:26 +0100

    Cleaning

--- src/connect4/Play.java
+++ src/connect4/Play.java
@@ -1,73 +1,47 @@
 package connect4;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Play {
 	
-//	private static final Scanner readCol = new Scanner(System.in);
+	private static final Scanner readCol = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		boolean hasPlayed;
 		Game game = new Game();
-		
-		game.displayGUI();
+
+		int move=0;
+
+		game.display();
 		
 		while (true) {
 			
 			do {
-//			System.out.print("Red's Turn : ");
-			hasPlayed = game.playRed(getColumnFromMouseClick());
+			System.out.print("Red's Turn : ");
+			move = readCol.nextInt();
+			hasPlayed = game.playRed(move);
 			}
 			while(!hasPlayed);
 			
-			game.displayGUI();
-			
 			if (game.checkWinRed()) {
-				
-				
-//				System.out.println("Red has won");
+				System.out.println("Red has won");
 				break;
 			}
 			
 			// Play Yellow
 			do {
-//			System.out.println("Yellow's Turn : ");
-			hasPlayed = game.playYellow((int)(Math.random() * 7));
+			move = (int)(Math.random() * 7);
+			System.out.println("Yellow's Turn : " + move);
+			hasPlayed = game.playYellow(move);
 			}
 			while(!hasPlayed);
 			
-			game.displayGUI();
-			
+			game.display();
+
 			if (game.checkWinYellow()) {
-//				System.out.println("Yellow has won");
+				System.out.println("Yellow has won");
 				break;
 			}
 		}
 	}
-	
-	public static int getColumnFromMouseClick() {
-      while (!StdDraw.isMousePressed()) {
-          // Wait for the mouse to be pressed
-      }
-      
-      while (StdDraw.isMousePressed()) {
-          // Keep looping until the mouse is released
-      }
-      
-      // Get the x-coordinate of the mouse click
-      double x = StdDraw.mouseX();
-
-      // Determine the column based on the x-coordinate
-      int column = (int) Math.round(x);
-
-      // Ensure the column is within bounds
-      if (column < 0) {
-          column = 0;
-      } else if (column >= 7) {
-          column = 7 - 1;
-      }
-
-      return column;
-  }
-
 }
